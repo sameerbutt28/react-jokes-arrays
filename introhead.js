2d7388b54dcb635f58901c677edf7acc9b1631bd
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-05-23 04:39:31 +0500

    Add files via upload

---
+++ introhead.js
@@ -0,0 +1,11 @@
+export default function IntroHead()
+{
+    return (
+        <div className="intro">
+<h1>
+    Jokes by Sameer - The Butt
+</h1>
+        </div>
+    )
+
+}
---
+++ jokessetup.js
@@ -0,0 +1,15 @@
+export default function Jokes(proop)
+{
+    return (
+        <div>
+             <h4>
+{ proop.setup}
+        </h4>
+        <p>
+{proop.punchline}
+        </p>
+        <hr/>
+        </div>
+    )
+
+}
